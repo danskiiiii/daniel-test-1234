187226d47beb818ad1229d8ceb03a9481d759b36
daniel <danielwil@wp.pl>
2019-03-21 11:07:36 +0100

    init

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.get('/home', (req,res)=> {
+res.send({
+  data:'hello world',
+});
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
